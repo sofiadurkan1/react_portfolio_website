21e1008c7389786ce87ed42eda785fde3eaf3d98
sofia durkan <sofiadurkan@gmail.com>
2021-08-17 09:58:18 -0500

    menu bar added

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import Portfolio from "./components/portfolio/Portfolio";
 import Testimonials from "./components/testimonials/Testimonials";
 import Works from "./components/works/Works";
 import Contact from "./components/contact/Contact";
+import Menu from "./components/menu/Menu";
+
 import './app.scss';
 import {useState} from 'react';
 
@@ -11,10 +13,12 @@ import {useState} from 'react';
 
 
 function App() {
-  const[menuOpen,setMenuOpen] = useState(true);
+  const[menuOpen,setMenuOpen] = useState(false);
   return (
     <div className="app">
       <Topbar menuOpen = {menuOpen} setMenuOpen={setMenuOpen}/>
+      <Menu menuOpen = {menuOpen} setMenuOpen={setMenuOpen}/>
+
       <div className="sections">
         <Intro/>
         <Portfolio/>
