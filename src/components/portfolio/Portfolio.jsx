2e327ccc53c4415acceb319345aca31e7901af45
sofia durkan <sofiadurkan@gmail.com>
2021-08-17 13:20:57 -0500

    portfolio page added

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -3,9 +3,34 @@ import './portfolio.scss';
 function Portfolio() {
     return (
         <div className="portfolio" id="portfolio">
+            <h1>Portfolio</h1>
+            <ul>
+                <li className="active">Featured</li>
+                <li>Web App</li>
+                <li>Design</li>
+                
+            </ul>
+            <div className="container">
+                <div className="item">
+                    <img src="" alt="" />
+                    <h3>Popular Tour Places</h3>
+                </div>
+                <div className="item">
+                    <img src="" alt="" />
+                    <h3>Popular Tour Places</h3>
+                </div>
+                <div className="item">
+                    <img src="" alt="" />
+                    <h3>Popular Tour Places</h3>
+                </div>
+                <div className="item">
+                    <img src="" alt="" />
+                    <h3>Popular Tour Places</h3>
+                </div>
+            </div>
             
         </div>
     )
 }
 
-export default Portfolio
+export default Portfolio;
